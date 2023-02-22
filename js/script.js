0c13b6db805af99dc1adc02e540fd9979a853c49
edvvarrd <edvvardsky@gmail.com>
2023-02-22 02:13:09 +0200

    Finished mobile nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navMobile = document.querySelector('.nav-mobile')
+const navMobileItems = document.querySelectorAll('.nav-mobile a')
 
 const showNav = () => {
-    navMobile.classList.toggle('show')
+	navMobile.classList.toggle('show')
 }
-
+const closeNav = () => {
+	navMobile.classList.remove('show')
+}
 navToggle.addEventListener('click', showNav)
+navMobileItems.forEach(e => e.addEventListener('click', closeNav))
