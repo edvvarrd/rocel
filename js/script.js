cf5ff16396d5fe8d5747c2809fa037364125cf8c
edvvarrd <edvvardsky@gmail.com>
2023-03-01 12:52:56 +0200

    First skeleton of projects section

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const navToggle = document.querySelector('.nav-toggle')
 const navMobile = document.querySelector('.nav-mobile')
 const navMobileItems = document.querySelectorAll('.nav-mobile a')
 
-const showNav = () => {
+const toggleNav = () => {
 	navMobile.classList.toggle('show')
 }
 const closeNav = () => {
 	navMobile.classList.remove('show')
 }
-navToggle.addEventListener('click', showNav)
+navToggle.addEventListener('click', toggleNav)
 navMobileItems.forEach(e => e.addEventListener('click', closeNav))
